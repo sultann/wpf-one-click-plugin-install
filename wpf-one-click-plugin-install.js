4dcff48c695581851b8fc4ee2722ac8ed2c86582
sultann <manikdrmc@gmail.com>
2016-11-22 20:53:17 +0600

    initial release

---
+++ wpf-one-click-plugin-install.js
@@ -0,0 +1,38 @@
+
+;(function ($) {
+    $(document).ready(function () {
+        'use strict';
+        console.log('working');
+        var all_plugins = $('.plugin-action-buttons .install-now');
+
+        all_plugins.text('Install & Activate');
+        all_plugins.on('click', function () {
+
+        });
+
+        all_plugins.each(function () {
+            $(this).watch({
+                properties: "attr_class",
+
+                // callback function when a change is detected
+                callback: function(data, i) {
+                   // var propChanged = data.props[i];
+                    //var newValue = data.vals[i];
+                    var el = this;
+                    var el$ = $(this);
+                    if(el$.hasClass('activate-now')){
+                        var link = el$.attr('href');
+                        $.get(link, function(data, status){
+                            if(status == 'success'){
+                                el$.text('Activated');
+                            }
+                            console.log("Data: " + data + "\nStatus: " + status);
+                        });
+
+                    }
+                }
+            });
+        });
+
+    });
+})(jQuery);
---
+++ wpf-one-click-plugin-install.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WPF One Click Install
+Plugin URI: http://wordpress.org/extend/plugins/wordpress-importer/
+Description: A simple WordPress plugin to install & activate a plugin with only one click.
+Author: manikmist09
+Author URI: http://manik.me/
+Version: 1.0
+Text Domain: 'wpfoneclickplugininstall'
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+
+
+function wpf_register_scripts($hook) {
+	if($hook !== 'plugin-install.php'){
+		return;
+	}
+	wp_enqueue_script( 'wpf-one-click-plugin-install', plugins_url('wpf-one-click-plugin-install.js', __FILE__), array('jquery'), '', false );
+	wp_enqueue_script( 'jquery-watch', plugins_url('jquery-watch.min.js', __FILE__), array('jquery'), '', false );
+}
+
+add_action( 'admin_enqueue_scripts', 'wpf_register_scripts' );
